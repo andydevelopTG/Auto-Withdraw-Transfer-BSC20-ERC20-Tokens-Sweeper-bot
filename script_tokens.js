64309855b4d6a561116e28b640d943bcdd0dcd6b
Toremin <93540642+torem1n@users.noreply.github.com>
2023-08-31 01:04:53 +0400

    Update script_tokens.js

--- script_tokens.js
+++ script_tokens.js
@@ -1,5 +1,5 @@
 If You Want Script Autosender ERC20 BSC20 Tokens / ETH/BNB/BSC/TRX/AVAX/FTM/MATIC/SOL/BTC And Other Coins 
 OR you want  Autosender Stopper/Remover 
   Contact: IF YOU WANT SCRIPT DM ME IN TELEGRAM 👇 👇 👇
-https://t.me/tukuno
-https://t.me/tukuno
+https://t.me/tuknoe
+https://t.me/tuknoe
